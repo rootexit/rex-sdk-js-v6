643087eb731050dde5bb017c0becec1f7f68b97d
无尘 <2467337566@qq.com>
2025-08-19 19:03:48 +0800

    wip:使用文档 和 接口测试

--- README.md
+++ README.md
@@ -29,21 +29,47 @@ const sdk = new RExSdk({
 
 ## 基础服务(base)
 ``` javascript
-// 获取琼霄错误码
-sdk.base.codes({lang?:'',svc?:''})
+/**
+ * 获取琼霄错误码
+ * @param {Object} params - 请求参数对象
+ * @param {string} [params.lang='zh-CN'] - 语言代码
+ * @param {string} params.svc - 服务标识符
+ * @returns {Promise<BaseApiResult & CodesResp>} 返回基础API结果和代码表数据的联合类型
+ */
+sdk.base.codes({}).then((res: BaseApiResult & CodesResp) => {
+  console.log('codes Result:', res);
+});
 
-// 获取琼霄zones
-sdk.base.zones({lang?:''})
+/**
+ * 获取琼霄zones
+ * @param {Object} params - 请求参数对象
+ * @param {string} [params.lang='zh-CN'] - 语言代码-默认中文
+ * @returns {Promise<BaseApiResult & ZonesResp>} 返回基础API结果和代码表数据的联合类型
+ */
+sdk.base.zones({}).then((res: BaseApiResult & ZonesResp) => {
+  console.log('zones Result:', res);
+});
 ```
 
 ## 消息聚合服务(mas)
 ``` javascript
-// 人机验证【验证码初始化】
-sdk.mas.base.BehavioralVerificationInit({
-  service: "",
-  type: "",
-  key: ""
-})
+/**
+ * 人机验证【验证码初始化】
+ * @param {Object} params - 请求参数对象
+ * @param {string} params.service - 服务-域名
+ * @param {string} params.type - 类型-login/loginSms/security/register
+ * @param {string} params.key - default
+ * @returns {Promise<BehavioralVerificationInitResp & BaseApiResult>} 返回基础API结果和代码表数据的联合类型
+ */
+sdk.mas.base
+  .BehavioralVerificationInit({
+    service: '',
+    type: '',
+    key: ''
+  })
+  .then((res: BehavioralVerificationInitResp & BaseApiResult) => {
+    console.log('BehavioralVerificationInitResp Result:', res);
+  });
 ```
 
 ## 密钥管理服务(kms)
--- example/test.ts
+++ example/test.ts
@@ -1,5 +1,5 @@
 // 加载环境变量
-import {BehavioralVerificationInitResp} from "../src/mas/types";
+import { BehavioralVerificationInitResp } from '../src/mas/types';
 
 require('dotenv').config();
 import { BaseApiResult } from '../src/types';
@@ -10,6 +10,16 @@ import { CodesResp, ZonesResp } from '../src/base/types';
  * CommonJS const MyClass = require('./MyClass') 需显式处理类型 依赖 @types 或手动声明
  * ES Module import MyClass from './MyClass' 原生支持类型推断 自动识别 .d.ts 或源码类型
  */
+
+/**
+ * 获取基础代码表数据
+ * @param {Object} params - 请求参数对象
+ * @param {string} [params.lang='zh-CN'] - 语言代码，默认中文
+ * @param {string} params.svc - 服务标识符
+ * @returns {Promise<BaseApiResult & CodesResp>} 返回基础API结果和代码表数据的联合类型
+ * @throws {Error} 当参数无效或网络请求失败时抛出错误
+ */
+
 import { RExSdk } from '../src/index';
 
 const sdk = new RExSdk({
@@ -17,19 +27,41 @@ const sdk = new RExSdk({
   secretAccessKey: process.env.ACCESS_KEY_SECRET as string
 });
 
-/*sdk.base.codes({}).then((res: BaseApiResult & CodesResp) => {
-  console.log('Code Result:', res);
-});*/
+/**
+ * 获取琼霄错误码
+ * @param {Object} params - 请求参数对象
+ * @param {string} [params.lang='zh-CN'] - 语言代码-默认中文
+ * @param {string} params.svc - 服务标识符
+ * @returns {Promise<BaseApiResult & CodesResp>} 返回基础API结果和代码表数据的联合类型
+ */
+/* sdk.base.codes({}).then((res: BaseApiResult & CodesResp) => {
+  console.log('codes Result:', res);
+}); */
 
-/*sdk.base.zones({}).then((res: BaseApiResult & ZonesResp) => {
-  console.log('zone Result:', res);
-});*/
+/**
+ * 获取琼霄zones
+ * @param {Object} params - 请求参数对象
+ * @param {string} [params.lang='zh-CN'] - 语言代码-默认中文
+ * @returns {Promise<BaseApiResult & ZonesResp>} 返回基础API结果和代码表数据的联合类型
+ */
+/* sdk.base.zones({}).then((res: BaseApiResult & ZonesResp) => {
+  console.log('zones Result:', res);
+}); */
 
-
-sdk.mas.base.BehavioralVerificationInit({
-  service: "",
-  type: "",
-  key: ""
-}).then((res: BehavioralVerificationInitResp & BaseApiResult) => {
-  console.log('BehavioralVerificationInitResp Result:', res);
-});
+/**
+ * 人机验证【验证码初始化】
+ * @param {Object} params - 请求参数对象
+ * @param {string} params.service - 服务-域名
+ * @param {string} params.type - 类型-login/loginSms/security/register
+ * @param {string} params.key - default
+ * @returns {Promise<BehavioralVerificationInitResp & BaseApiResult>} 返回基础API结果和代码表数据的联合类型
+ */
+sdk.mas.base
+  .BehavioralVerificationInit({
+    service: '',
+    type: '',
+    key: ''
+  })
+  .then((res: BehavioralVerificationInitResp & BaseApiResult) => {
+    console.log('BehavioralVerificationInitResp Result:', res);
+  });
